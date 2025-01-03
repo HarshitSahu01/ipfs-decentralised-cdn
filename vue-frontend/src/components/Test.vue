805160327f82c310e9cfadb31909f6aeac467436
HarshitSahu01 <sahuharshit2019@gmail.com>
2025-01-03 14:30:03 +0530

    modified:   vue-frontend/src/App.vue 	deleted:    vue-frontend/src/Pages/AboutPage.vue 	modified:   vue-frontend/src/Pages/HomePage.vue 	modified:   vue-frontend/src/assets/base.css 	deleted:    vue-frontend/src/assets/logo.svg 	modified:   vue-frontend/src/assets/main.css 	deleted:    vue-frontend/src/components/HelloWorld.vue 	new file:   vue-frontend/src/components/Test.vue 	deleted:    vue-frontend/src/components/TheWelcome.vue 	deleted:    vue-frontend/src/components/WelcomeItem.vue 	modified:   vue-frontend/src/router/index.js

---
+++ vue-frontend/src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+    Hello World {{ age }} {{ color }}
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data() {
+        return {
+            age: 18
+        }
+    },
+    computed(){
+        return {
+            color: 'pink'
+        }
+    }
+}
+</script>
