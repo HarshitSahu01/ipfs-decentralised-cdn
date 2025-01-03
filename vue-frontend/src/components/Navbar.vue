893f7f2d150715bdb21d83216572b3b1353e7692
HarshitSahu01 <sahuharshit2019@gmail.com>
2025-01-03 21:47:17 +0530

    modified:   src/components/Navbar.vue

--- vue-frontend/src/components/Navbar.vue
+++ vue-frontend/src/components/Navbar.vue
@@ -1,15 +1,18 @@
 <template>
   <nav class="flex items-center sticky top-0 z-50 justify-between px-6 py-4 bg-white shadow-md">
     <!-- Logo Section -->
-    <div class="flex items-center space-x-3">
-      <img src="../assets/folder-svgrepo-com.svg" alt="logo" />
-      <span class="text-lg font-semibold">IPFS</span>
-    </div>
-    
+    <RouterLink to="/">
+      <div class="flex items-center space-x-3">
+        <img src="../assets/folder-svgrepo-com.svg" alt="logo" />
+        <span class="text-lg font-semibold">IPFS</span>
+      </div>
+    </RouterLink>
+
     <!-- Navigation Links -->
     <div class="flex items-center space-x-4">
       <template v-if="isLoggedIn">
-        <a href="/dashboard" class="px-4 py-2 text-gray-600 border rounded-md hover:text-gray-900">Dashboard</a>
+        <RouterLink to="/dashboard" class="px-4 py-2 text-gray-600 border rounded-md hover:text-gray-900">Dashboard
+        </RouterLink>
         <button @click="logout" class="px-4 py-2 text-gray-600 border rounded-md hover:text-gray-900">Logout</button>
       </template>
       <button v-else @click="login" class="px-4 py-2 text-gray-600 border rounded-md hover:text-gray-900">Login</button>
@@ -18,6 +21,7 @@
 </template>
 
 <script>
+import { RouterLink } from 'vue-router';
 export default {
   name: "NavbarComponent",
   data() {
