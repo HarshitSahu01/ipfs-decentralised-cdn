09c5d98670d0ca5c417d665b0b64b6cc328739e6
HarshitSahu01 <sahuharshit2019@gmail.com>
2025-01-20 16:58:50 +0530

    new file:   backend/auth.js 	new file:   backend/fileManager.js 	modified:   backend/index.js

---
+++ backend/auth.js
@@ -0,0 +1,36 @@
+import express from 'express'
+const router = express.Router()
+
+import "./firebase.js";
+import { getAuth, signInWithCredential, GoogleAuthProvider,} from "firebase/auth";
+const auth = getAuth();
+
+router.post("/api/login", (req, res) => {
+  // console.log(req.body.credential);
+  const credential = GoogleAuthProvider.credential(req.body.credential);
+  signInWithCredential(auth, credential)
+  .then(data => console.log(`user logged in ${auth.currentUser.uid}`))
+  .catch((error) => {
+    
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(errorMessage)
+  });
+  res.send("hi");
+});
+
+export default router;
+
+/* 
+1. If user is new or existing
+if existing, just say login successfuly
+if new, create a user document in users collection in firestore
+
+validateUser(uid)
+
+createShortLink (uid, fileId)
+1. Read the file hash
+2. map the short key to the hash
+ */
--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from './auth.js';
 
 dotenv.config();
 const app = express();
 app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
 app.use(express.json());
 
-import "./firebase.js";
-
-import { getAuth, signInWithCredential, GoogleAuthProvider,} from "firebase/auth";
-const auth = getAuth();
+app.use('/', authRoutes);
 
 app.get("/", (req, res) => {
   res.send({msg: "API is running"+"\n meow meow sena\n\n"+auth.currentUser.uid})
-});
-
-app.post("/api/login", (req, res) => {
-  // console.log(req.body.credential);
-  const credential = GoogleAuthProvider.credential(req.body.credential); // Create login
-  signInWithCredential(auth, credential).then(data => console.log(`user logged in ${auth.currentUser.uid}`)).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    console.log(errorMessage)
-    // ...
-  });
-  res.send("hi");
 });
 
 const PORT = process.env.PORT || 5000;
