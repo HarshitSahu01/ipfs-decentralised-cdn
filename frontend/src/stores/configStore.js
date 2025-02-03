2406f1f84b3949e817fcd0858be2e4add521b336
HarshitSahu01 <sahuharshit2019@gmail.com>
2025-02-03 20:15:32 +0530

    fixed package.json

--- frontend/src/stores/configStore.js
+++ frontend/src/stores/configStore.js
@@ -1,15 +1,9 @@
 import { defineStore } from "pinia";
 
 export const configStore = defineStore('configStore',{
-    state: () => {
-        renderURL: 'https://express-backend-2b4s.onrender.com'
-    },
-
     actions:{
         backendURL(){
-            if(location.hostname === 'localhost')
-                return 'https://express-backend-2b4s.onrender.com'
-            return this.renderURL
+            return process.env.BACKEND_URL
         }
     }
 })
